89c08db95593eb1666bbdd8a49ad17241e03f33e
Matthew Mongeau <halogenandtoast@gmail.com>
2012-10-01 17:00:00 -0400

    Followed user relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   include Clearance::User
   has_many :shouts
 
+  # follower_id, followed_user_id
+  #
+  has_many :following_relationships, foreign_key: :follower_id
+  has_many :followed_users, through: :following_relationships
+
   def can_follow? user
     user != self
   end
