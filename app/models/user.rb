d24dd59fabfb2324f3dffaffac39e62d1f46f81d
Matthew Mongeau <halogenandtoast@gmail.com>
2012-10-02 10:55:57 -0400

    Follower relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ActiveRecord::Base
   include Clearance::User
   has_many :shouts
+  has_many :followed_user_relationships, foreign_key: :follower_id, class_name: 'FollowingRelationship'
+  has_many :followed_users, through: :followed_user_relationships
 
-  # follower_id, followed_user_id
-  #
-  has_many :following_relationships, foreign_key: :follower_id
-  has_many :followed_users, through: :following_relationships
+  has_many :follower_relationships, foreign_key: :followed_user_id, class_name: 'FollowingRelationship'
+  has_many :followers, through: :follower_relationships
 
   def can_follow? user
-    user != self
+    self_and_followed_user_ids.exclude?(user.id)
+  end
+
+  private
+
+  def self_and_followed_user_ids
+    followed_user_ids + [id]
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,20 @@ require 'spec_helper'
 
 describe User do
   it "#can_follow? unless same user" do
-    user = build(:user)
+    user = build_stubbed(:user)
     user.can_follow?(user).should eq(false)
   end
 
   it "#can_follow? unless already following" do
-
+    follower = create(:user)
+    followed_user = create(:user)
+    follower.followed_users << followed_user
+    follower.can_follow?(followed_user).should eq(false)
   end
 
   it "#can_follow? other users" do
-    follower = build(:user)
-    user_to_follow = build(:user)
+    follower = build_stubbed(:user)
+    user_to_follow = build_stubbed(:user)
     follower.can_follow?(user_to_follow).should eq(true)
   end
 end
